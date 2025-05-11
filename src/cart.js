d8674877d77fa7c80480e9c020bcd0de3c39e5d0
elen-purple <anna.havrylova.2011@gmail.com>
2025-05-11 20:37:34 +0300

    makes basic structure

---
+++ src/cart.js
@@ -0,0 +1,3 @@
+import "./js/cart.js";
+import "./js/order.js";
+import "./js/subscribe.js";
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./js/filter.js"
+import "./js/products.js"
+import "./js/popular.js"
+import "./js/discount.js"
+import "./js/pagination.js"
+import "./js/subscribe.js";
+import "./js/cart.js";
+import "./js/order.js";
