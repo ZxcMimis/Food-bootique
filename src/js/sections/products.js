5259685a42667b7cbac6583ec2141e88d640b938
Руслан Івасюк <126970988+ZxcMimis@users.noreply.github.com>
2025-05-31 14:32:28 +0300

    fix

---
+++ src/js/sections/products.js
@@ -0,0 +1,25 @@
+// import { getProduct } from "../fetchs/getProduct";
+
+// const createProductsList = (products) =>
+//     products.map((product) =>
+//         `
+//     <li class="products__item">
+//                 <div class="products__container_img">
+//                     <img src="./img/destop/x1/avocado@1x.webp" alt="Carrots" class="products__img">
+//                 </div>
+//                 <h2 class="products__title">Cabbage</h2>
+//                 <p class="products__category">Category: <span>Fresh Produce</span></p>
+//                 <p class="products__size">Size: <span>per head</span></p>
+//                 <p class="products__popularity">Popularity: <span>0</span></p>
+//                 <div class="products__svg_price">
+//                     <p class="products__price">$1.99</p>
+//                     <div class="products__svg_container">
+//                         <svg class="products__basket">
+//                             <use href="./svg/icons.svg#cart"></use>
+//                         </svg>
+//                     </div>
+
+//                 </div>
+
+//             </li>
+//     `)
