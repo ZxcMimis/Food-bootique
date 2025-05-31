ee98a6b477e38b2ee95fa0cf916c719036d46e63
elen-purple <anna.havrylova.2011@gmail.com>
2025-05-31 18:41:23 +0300

    Updates

--- cart.16893d70.js
+++ cart.16893d70.js
@@ -808,11 +808,18 @@ exports.export = function(dest, destName, get) {
 
 },{}],"57Le5":[function(require,module,exports,__globalThis) {
 const backdrop = document.querySelector(".order__modal");
-const orderButton = document.querySelector(".order-button");
+const form = document.querySelector(".cart__form");
 const closeButton = document.querySelector(".order__close-btn");
-orderButton.addEventListener("click", ()=>{
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
     backdrop.classList.toggle("is-hidden");
     document.body.style.overflow = "hidden";
+    window.addEventListener("keydown", (event)=>{
+        if (event.key === "Escape") {
+            backdrop.classList.add("is-hidden");
+            document.body.style.overflow = "visible";
+        }
+    });
 });
 backdrop.addEventListener("click", (event)=>{
     if (event.target === backdrop) {
