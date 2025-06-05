05459719ad8a10bcb99c53bfa4607933d7d7a28a
Руслан Івасюк <126970988+ZxcMimis@users.noreply.github.com>
2025-06-05 18:37:24 +0300

    producuts

--- src/js/sections/products.js
+++ src/js/sections/products.js
@@ -1,25 +1,59 @@
-// import { getProduct } from "../fetchs/getProduct";
+import { getFilteredProducts } from "../fetchs/getFilteredProducts";
 
-// const createProductsList = (products) =>
-//     products.map((product) =>
-//         `
-//     <li class="products__item">
-//                 <div class="products__container_img">
-//                     <img src="./img/destop/x1/avocado@1x.webp" alt="Carrots" class="products__img">
-//                 </div>
-//                 <h2 class="products__title">Cabbage</h2>
-//                 <p class="products__category">Category: <span>Fresh Produce</span></p>
-//                 <p class="products__size">Size: <span>per head</span></p>
-//                 <p class="products__popularity">Popularity: <span>0</span></p>
-//                 <div class="products__svg_price">
-//                     <p class="products__price">$1.99</p>
-//                     <div class="products__svg_container">
-//                         <svg class="products__basket">
-//                             <use href="./svg/icons.svg#cart"></use>
-//                         </svg>
-//                     </div>
+getFilteredProducts("", "", 1, "").then((data) => {
 
-//                 </div>
+    document.querySelector("#products-list").innerHTML = data.results
+        .map(
+            ({
+                _id,
+                name,
+                img,
+                category,
+                size,
+                price,
+                is10PercentOff,
+                popularity,
+            }) => `
+    <li id="${_id}" data-product="true" class="products__item">
+        <div class="products__container_img">
+            <img src="${img}" alt="Carrots" class="products__img">
+        </div>
+        <h2 class="products__title">${name}</h2>
+        <p class="products__category">Category: <span>${category}</span></p>
+        <p class="products__size">Size: <span>${size}</span></p>
+        <p class="products__popularity">Popularity: <span>${popularity}</span></p>
+        <div class="products__svg_price">
+        <p class="products__price">$${price}</p>
+            <button class="products__svg_btn">
+                <svg class="products__basket">
+                    <use href="#cart"></use>
+                </svg>
+            </button>
+        </div>
+    </li>
+    `
+        )
+        .join("");
 
-//             </li>
-//     `)
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
