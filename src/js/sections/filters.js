4840997f792a42c64122de3c6d7564343d357441
elen-purple <anna.havrylova.2011@gmail.com>
2025-06-06 17:00:27 +0300

    makes discount js

--- src/js/sections/filters.js
+++ src/js/sections/filters.js
@@ -50,10 +50,19 @@ const makeMarkup = (keyword, category, id, sort) => {
           is10PercentOff,
           popularity,
         }) => `<li id="${_id}" data-product="true" class="products__item">
+        ${
+          is10PercentOff
+            ? `<div class="products__green">
+  <svg class="products__discount" width="60" height="60">
+    <use href="#discount"></use>
+  </svg>
+</div>`
+            : ""
+        }
                 <div class="products__container_img">
                     <img src="${img}" alt="${name}" class="products__img">
                 </div>
-                <h2 class="products__title">Avocado</h2>
+                <h2 class="products__title">${name}</h2>
                 <p class="products__category">Category: <span>${category}</span></p>
                 <p class="products__size">Size: <span>${size}</span></p>
                 <p class="products__popularity">Popularity: <span>${popularity}</span></p>
