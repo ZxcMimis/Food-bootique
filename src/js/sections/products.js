17a4d56ac2534e1245ab3eaa981d4bc5143c2f36
Руслан Івасюк <126970988+ZxcMimis@users.noreply.github.com>
2025-06-07 00:35:03 +0300

    половина js products зделана

--- src/js/sections/products.js
+++ src/js/sections/products.js
@@ -1,31 +1,25 @@
 import { getFilteredProducts } from "../fetchs/getFilteredProducts";
 
-getFilteredProducts("", "", 1, "").then((data) => {
 
-    document.querySelector("#products-list").innerHTML = data.results
+
+const productsListContainer = document.querySelector("#products-list");
+
+getFilteredProducts("", "", 1, "").then((data) => {
+    productsListContainer.innerHTML = data.results
         .map(
-            ({
-                _id,
-                name,
-                img,
-                category,
-                size,
-                price,
-                is10PercentOff,
-                popularity,
-            }) => `
-    <li id="${_id}" data-product="true" class="products__item">
+            ({ _id, name, img, category, size, price, popularity }) => `
+    <li id="${_id}" class="products__item">
         <div class="products__container_img">
-            <img src="${img}" alt="Carrots" class="products__img">
+            <img src="${img}" alt="${name}" class="products__img">
         </div>
         <h2 class="products__title">${name}</h2>
         <p class="products__category">Category: <span>${category}</span></p>
         <p class="products__size">Size: <span>${size}</span></p>
         <p class="products__popularity">Popularity: <span>${popularity}</span></p>
         <div class="products__svg_price">
-        <p class="products__price">$${price}</p>
-            <button class="products__svg_btn">
-                <svg class="products__basket">
+            <p class="products__price">$${price}</p>
+            <button class="products__svg_btn add-to-cart-btn">
+                <svg class="products__basket" width="18" height="18">
                     <use href="#cart"></use>
                 </svg>
             </button>
@@ -34,7 +28,23 @@ getFilteredProducts("", "", 1, "").then((data) => {
     `
         )
         .join("");
+});
 
+
+productsListContainer.addEventListener('click', (event) => {
+    const clickedButton = event.target.closest('.add-to-cart-btn');
+    if (!clickedButton) {
+        return;
+    }
+
+
+    const useElement = clickedButton.querySelector('use');
+    if (useElement) {
+        const currentIcon = useElement.getAttribute('href');
+        if (currentIcon === '#cart') {
+            useElement.setAttribute('href', '#check'); 
+        }
+    }
 });
 
 
