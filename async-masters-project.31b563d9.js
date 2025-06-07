15d1e2fc923d1dec1a2bc174f6d7aea8dadfbe86
elen-purple <anna.havrylova.2011@gmail.com>
2025-06-07 11:09:49 +0300

    Updates

--- async-masters-project.31b563d9.js
+++ async-masters-project.31b563d9.js
@@ -677,26 +677,36 @@ var _subscribeJs = require("./js/sections/subscribe.js");
 
 },{"./js/sections/products.js":"7kVSa","./js/sections/discount.js":"2Dntu","./js/sections/pagination.js":"drJWK","./js/sections/subscribe.js":"4utt9","./js/sections/popular.js":"aJ9bM","./js/sections/product.js":"3SG3N","./js/sections/filters.js":"8U2Ig"}],"7kVSa":[function(require,module,exports,__globalThis) {
 var _getFilteredProducts = require("../fetchs/getFilteredProducts");
+const productsListContainer = document.querySelector("#products-list");
 (0, _getFilteredProducts.getFilteredProducts)("", "", 1, "").then((data)=>{
-    document.querySelector("#products-list").innerHTML = data.results.map(({ _id, name, img, category, size, price, is10PercentOff, popularity })=>`
-    <li id="${_id}" data-product="true" class="products__item">
+    productsListContainer.innerHTML = data.results.map(({ _id, name, img, category, size, price, popularity })=>`
+    <li id="${_id}" class="products__item">
         <div class="products__container_img">
-            <img src="${img}" alt="Carrots" class="products__img">
+            <img src="${img}" alt="${name}" class="products__img">
         </div>
         <h2 class="products__title">${name}</h2>
         <p class="products__category">Category: <span>${category}</span></p>
         <p class="products__size">Size: <span>${size}</span></p>
         <p class="products__popularity">Popularity: <span>${popularity}</span></p>
         <div class="products__svg_price">
-        <p class="products__price">$${price}</p>
-            <button class="products__svg_btn">
-                <svg class="products__basket">
+            <p class="products__price">$${price}</p>
+            <button class="products__svg_btn add-to-cart-btn">
+                <svg class="products__basket" width="18" height="18">
                     <use href="#cart"></use>
                 </svg>
             </button>
         </div>
     </li>
     `).join("");
+});
+productsListContainer.addEventListener('click', (event)=>{
+    const clickedButton = event.target.closest('.add-to-cart-btn');
+    if (!clickedButton) return;
+    const useElement = clickedButton.querySelector('use');
+    if (useElement) {
+        const currentIcon = useElement.getAttribute('href');
+        if (currentIcon === '#cart') useElement.setAttribute('href', '#check');
+    }
 });
 
 },{"../fetchs/getFilteredProducts":"cf4nK"}],"cf4nK":[function(require,module,exports,__globalThis) {
