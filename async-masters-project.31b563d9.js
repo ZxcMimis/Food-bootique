08f73efc372b2021799d380b75954caa4f8e6eca
elen-purple <anna.havrylova.2011@gmail.com>
2025-06-07 14:14:56 +0300

    Updates

--- async-masters-project.31b563d9.js
+++ async-masters-project.31b563d9.js
@@ -888,7 +888,7 @@ var _getDiscountProducts = require("../fetchs/getDiscountProducts");
 (0, _getDiscountProducts.getDiscountProducts)().then((products)=>{
     let markUp = "";
     for(let index = 0; index < 2; index += 1)markUp += `
-             <li id='${products[index]._id}' class="discount__item">
+             <li id='${products[index]._id}' class="discount__item" data-product="true">
             <svg class="discount__svg-discount">
               <use href="#discount"></use>
             </svg>
@@ -903,11 +903,12 @@ var _getDiscountProducts = require("../fetchs/getDiscountProducts");
               <h3 class="discount__item-title">${products[index].name}</h3>
               <div class="discount__item-wrap">
                 <p class="discount__price">$${products[index].price}</p>
-                <div class="discount__svg-container">
-                  <svg class="discount__basket">
-                    <use href="#cart"></use>
-                  </svg>
-                </div>
+                <button data-productadd="true" class="discount__svg-container">
+                    ${JSON.parse(localStorage.getItem("cart")).map((item)=>item.id).includes(products[index]._id) ? "\u2713" : `
+                        <svg class="discount__basket">
+                        <use href="#cart"></use>
+                        </svg>`}
+                    </button>
               </div>
             </div>
           </li>
