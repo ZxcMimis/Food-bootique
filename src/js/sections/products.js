e63483d4cdac6cc43376d766ef78b6cd6e3c4fec
Руслан Івасюк <126970988+ZxcMimis@users.noreply.github.com>
2025-06-07 13:48:54 +0300

    js products full

--- src/js/sections/products.js
+++ src/js/sections/products.js
@@ -5,47 +5,58 @@ import { getFilteredProducts } from "../fetchs/getFilteredProducts";
 const productsListContainer = document.querySelector("#products-list");
 
 getFilteredProducts("", "", 1, "").then((data) => {
-    productsListContainer.innerHTML = data.results
+    document.querySelector("#products-list").innerHTML = data.results
         .map(
-            ({ _id, name, img, category, size, price, popularity }) => `
-    <li id="${_id}" class="products__item">
-        <div class="products__container_img">
-            <img src="${img}" alt="${name}" class="products__img">
-        </div>
-        <h2 class="products__title">${name}</h2>
-        <p class="products__category">Category: <span>${category}</span></p>
-        <p class="products__size">Size: <span>${size}</span></p>
-        <p class="products__popularity">Popularity: <span>${popularity}</span></p>
-        <div class="products__svg_price">
-            <p class="products__price">$${price}</p>
-            <button class="products__svg_btn add-to-cart-btn">
-                <svg class="products__basket" width="18" height="18">
-                    <use href="#cart"></use>
-                </svg>
-            </button>
-        </div>
-    </li>
-    `
-        )
-        .join("");
-});
+            ({
+                _id,
+                name,
+                img,
+                category,
+                size,
+                price,
+                is10PercentOff,
+                popularity,
+            }) => `
+            <li id="${_id}" data-product="true" class="products__item">
 
-
-productsListContainer.addEventListener('click', (event) => {
-    const clickedButton = event.target.closest('.add-to-cart-btn');
-    if (!clickedButton) {
-        return;
-    }
-
-
-    const useElement = clickedButton.querySelector('use');
-    if (useElement) {
-        const currentIcon = useElement.getAttribute('href');
-        if (currentIcon === '#cart') {
-            useElement.setAttribute('href', '#check'); 
+${
+          is10PercentOff
+            ? `<div class="products__green">
+  <svg class="products__discount" width="60" height="60">
+    <use href="#discount"></use>
+  </svg>
+</div>`
+            : ""
         }
-    }
+
+            <div class= "products__container_img" >
+                    <img src="${img}" alt="${name}" class="products__img">
+                </div>
+                <h2 class="products__title">${name}</h2>
+                <p class="products__category">Category: <span>${category}</span></p>
+                <p class="products__size">Size: <span>${size}</span></p>
+                <p class="products__popularity">Popularity: <span>${popularity}</span></p>
+                <div class="products__svg_price">
+                    <p class="products__price">$${price}</p>
+                    <button  data-productadd="true" class="products__svg_btn">
+                    ${JSON.parse(localStorage.getItem("cart"))
+                    .map((item) => item.id)
+                    .includes(_id)
+                    ? "✓"
+                    :
+                    `<svg class="products__basket">
+                        <use href="#cart"></use>
+                    </svg>`
+                }
+                    </button>
+                </div>
+            </li>`
+        )
+
+        .join("")
 });
+
+
 
 
 
